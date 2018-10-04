6e72bc8545c342a833dc6021e13ebf40982e14a3
chrism <Christopher.Micallef@tipico.com>
2018-10-04 21:37:29 +0200

    Added Black and BlockChain classes

--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,12 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar!!', 'zoo', 'baz');
+// const block = new Block('foo', 'bar!!!!!!', 'zoo', 'this is the data');
+//
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
 
-console.log(block.toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+const fightersBlock = Block.mineBlock(fooBlock, 'fighters');
+
+console.log(fooBlock.toString());
+console.log(fightersBlock.toString());
